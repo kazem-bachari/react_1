6dcd81ad9db0e5270ba6824058b42f3a26edbc72
w3schools-test <test@w3schools.com>
2021-11-29 09:41:04 +0330

    file configured

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import 'jquery.js';
+import 'animate.css';
+import Carousel from './components/carousel/carousel';
 
+const { JSDOM } = require("jsdom");
+const { window } = new JSDOM("");
+const $ = require("jquery")(window);
 function App() {
+  const categoris = [
+    {id:1,categoty:'pc'},
+    {id:2,categoty:'phone'},
+    {id:3,categoty:'laptob'},
+    {id:4,categoty:'tablet'},
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        {categoris.map((category) =>
+          <p className={`bg-blue-400 capitalize m-3 p-2 animate__bounceInLeft  text-white  tracking-widest text-center cursor-pointer hover:bg-red-400 }}} transition-all duration-500  ${category.id == 1 ? 'border border-3' : null}}`}>{category.categoty}</p>
+        )}
+      </div>
+      <Carousel />
+    </>
   );
+ 
 }
 
 export default App;
---
+++ src/components/carousel/carousel.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Carousel = () => {
+
+    return (
+        <div>
+            <p className=''>hi</p>
+        </div>
+    );
+}
+
+export default Carousel;
